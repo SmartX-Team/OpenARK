25e34fdbb2c6fabe36199581d611950a611ecce3
Ho Kim <ho.kim@ulagbulag.io>
2022-06-04 18:26:43 +0000

    Add matchbox rpc parser

---
+++ kiss/matchbox/api/src/pb.rs
@@ -0,0 +1,9 @@
+pub(crate) mod rpcpb {
+    ::tonic::include_proto!("rpcpb");
+}
+pub(crate) mod serverpb {
+    ::tonic::include_proto!("serverpb");
+}
+pub(crate) mod storagepb {
+    ::tonic::include_proto!("storagepb");
+}
