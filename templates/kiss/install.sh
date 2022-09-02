2178b727c1915cf36f5c81b85610cb272fb2a133
Ho Kim <ho.kim@ulagbulag.io>
2022-09-02 14:35:32 +0000

    Detach ansible task files from configmap

--- templates/kiss/install.sh
+++ templates/kiss/install.sh
@@ -14,7 +14,18 @@ kubectl apply \
     -f ntpd.yaml
 
 # ansible tasks
-kubectl apply -R -f "./tasks"
+kubectl apply -f ./tasks/common.yaml
+for dir in ./tasks/*; do
+    # playbook directory
+    if [ -d "$dir" ]; then
+        kubectl create configmap "ansible-task-$(basename $dir)" \
+            --namespace=kiss \
+            --from-file=$dir \
+            --output=yaml \
+            --dry-run=client |
+            kubectl apply -f -
+    fi
+done
 
 # kiss service
 kubectl apply -R -f "./kiss-*.yaml"
@@ -22,4 +33,4 @@ kubectl apply -R -f "./kiss-*.yaml"
 # force rolling-update kiss services
 # note: https://github.com/kubernetes/kubernetes/issues/27081#issuecomment-327321981
 kubectl patch -R -f "./kiss-*.yaml" -p \
-    "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"updatedDate\":\"`date +'%s'`\"}}}}}"
+    "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"updatedDate\":\"$(date +'%s')\"}}}}}"
