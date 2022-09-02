b97135dc0ecbc1a496972f5e732bd65155b257c8
Ho Kim <ho.kim@ulagbulag.io>
2022-09-02 06:24:11 +0000

    Support force rolling-update kiss services

--- templates/kiss/install.sh
+++ templates/kiss/install.sh
@@ -14,12 +14,12 @@ kubectl apply \
     -f ntpd.yaml
 
 # ansible tasks
-kubectl apply -R -f tasks
+kubectl apply -R -f "./tasks"
 
 # kiss service
-kubectl apply \
-    -f kiss-assets.yaml \
-    -f kiss-controller.yaml \
-    -f kiss-gateway.yaml \
-    -f kiss-manager.yaml \
-    -f kiss-monitor.yaml
+kubectl apply -R -f "./kiss-*.yaml"
+
+# force rolling-update kiss services
+# note: https://github.com/kubernetes/kubernetes/issues/27081#issuecomment-327321981
+kubectl patch -R -f "./kiss-*.yaml" -p \
+    "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"updatedDate\":\"`date +'%s'`\"}}}}}"
