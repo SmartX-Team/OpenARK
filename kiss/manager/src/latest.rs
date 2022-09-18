fab93553ad9dbd00ec9ee9f457c8c2385f4b33c8
Ho Kim <ho.kim@ulagbulag.io>
2022-09-18 07:01:07 +0000

    Fix a bug in downloading versions

--- kiss/manager/src/latest.rs
+++ kiss/manager/src/latest.rs
@@ -27,14 +27,30 @@ impl Handler {
     const REPOSITORY_NAME: &'static str = "netai-cloud";
     const REPOSITORY_OWNER: &'static str = "ulagbulag-village";
 
+    const MAX_RETRY: usize = 5;
+
     pub async fn get_version(&self) -> Result<Version> {
         // request the latest release info
-        let release = self
-            .instance
-            .repos(&self.repo_owner, &self.repo_name)
-            .releases()
-            .get_latest()
-            .await?;
+        let release = 'load_release: loop {
+            for retry in 0..Self::MAX_RETRY {
+                match self
+                    .instance
+                    .repos(&self.repo_owner, &self.repo_name)
+                    .releases()
+                    .get_latest()
+                    .await
+                {
+                    Ok(release) => break 'load_release release,
+                    Err(e) => {
+                        if retry + 1 == Self::MAX_RETRY {
+                            return Err(e.into());
+                        } else {
+                            continue;
+                        }
+                    }
+                }
+            }
+        };
 
         // compare with the current release tag
         if !release.tag_name.starts_with('v') {
