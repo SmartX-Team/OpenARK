88c5209956a1b39ef32d07c0aca15ff392f3ed4e
Ho Kim <ho.kim@ulagbulag.io>
2022-09-25 20:13:29 +0000

    Add LICENSE to snapshot command

--- templates/snapshot/git/upload.sh
+++ templates/snapshot/git/upload.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2022 Ho Kim (ho.kim@ulagbulag.io). All rights reserved.
+# Use of this source code is governed by a GPL-3-style license that can be
+# found in the LICENSE file.
 
 # Prehibit errors
 set -e
