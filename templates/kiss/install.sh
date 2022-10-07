cad45709d8a3e02b302262fd1a4a4dd78a2efd28
Ho Kim <ho.kim@ulagbulag.io>
2022-10-07 22:00:07 +0000

    Add support of deploying CSI to child clusters

--- templates/kiss/install.sh
+++ templates/kiss/install.sh
@@ -12,57 +12,63 @@ set -x
 #   Configuration                                         #
 ###########################################################
 
+# Configure default environment variables
+INSTALL_KISS_DEFAULT="true"
+
 # Get environment variables
 BAREMETAL_CSI=$(
     kubectl get -n kiss configmap baremetal \
         -o jsonpath --template '{.data.csi}'
 )
+INSTALL_KISS="${INSTALL_KISS:-$INSTALL_KISS_DEFAULT}"
 
 ###########################################################
 #   Install Kiss Cluster                                  #
 ###########################################################
 
-echo "- Installing kiss cluster ..."
+if [ "$INSTALL_KISS_DEFAULT" == "true" ]; then
+    echo "- Installing kiss cluster ..."
 
-# namespace & common
-kubectl apply \
-    -f namespace.yaml
+    # namespace & common
+    kubectl apply \
+        -f namespace.yaml
 
-# services
-kubectl apply \
-    -f dnsmasq.yaml \
-    -f docker-registry.yaml \
-    -f http-proxy.yaml \
-    -f matchbox.yaml \
-    -f ntpd.yaml
+    # services
+    kubectl apply \
+        -f dnsmasq.yaml \
+        -f docker-registry.yaml \
+        -f http-proxy.yaml \
+        -f matchbox.yaml \
+        -f ntpd.yaml
 
-# ansible tasks
-kubectl apply -f ./tasks/common.yaml
-for dir in ./tasks/*; do
-    # playbook directory
-    if [ -d "$dir" ]; then
-        kubectl create configmap "ansible-task-$(basename $dir)" \
-            --namespace=kiss \
-            --from-file=$dir \
-            --output=yaml \
-            --dry-run=client |
-            kubectl apply -f -
-    fi
-done
+    # ansible tasks
+    kubectl apply -f ./tasks/common.yaml
+    for dir in ./tasks/*; do
+        # playbook directory
+        if [ -d "$dir" ]; then
+            kubectl create configmap "ansible-task-$(basename $dir)" \
+                --namespace=kiss \
+                --from-file=$dir \
+                --output=yaml \
+                --dry-run=client |
+                kubectl apply -f -
+        fi
+    done
 
-# power configuration
-kubectl apply -R -f "./power/*.yaml"
+    # power configuration
+    kubectl apply -R -f "./power/*.yaml"
 
-# kiss service
-kubectl apply -R -f "./kiss-*.yaml"
+    # kiss service
+    kubectl apply -R -f "./kiss-*.yaml"
 
-# snapshot configuration
-kubectl apply -R -f "./snapshot-*.yaml"
+    # snapshot configuration
+    kubectl apply -R -f "./snapshot-*.yaml"
 
-# force rolling-update kiss services
-# note: https://github.com/kubernetes/kubernetes/issues/27081#issuecomment-327321981
-kubectl patch -R -f "./kiss-*.yaml" -p \
-    "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"updatedDate\":\"$(date +'%s')\"}}}}}"
+    # force rolling-update kiss services
+    # note: https://github.com/kubernetes/kubernetes/issues/27081#issuecomment-327321981
+    kubectl patch -R -f "./kiss-*.yaml" -p \
+        "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"updatedDate\":\"$(date +'%s')\"}}}}}"
+fi
 
 ###########################################################
 #   Install Bare-metal CSI                                #
