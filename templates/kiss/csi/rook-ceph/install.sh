dcee8ca1970dd93ff58f6d95d885c0dc18b25485
Ho Kim <ho.kim@ulagbulag.io>
2022-10-21 12:28:54 +0000

    Try to fix a rook-ceph CSI issue

--- templates/kiss/csi/rook-ceph/install.sh
+++ templates/kiss/csi/rook-ceph/install.sh
@@ -14,9 +14,11 @@ set -x
 
 # Configure default environment variables
 HELM_CHART_DEFAULT="https://charts.rook.io/release"
+USE_SINGLE_MON_UNTIL_DEPLOYED_DEFAULT="true"
 
 # Set environment variables
 HELM_CHART="${HELM_CHART:-$HELM_CHART_DEFAULT}"
+USE_SINGLE_MON_UNTIL_DEPLOYED="${USE_SINGLE_MON_UNTIL_DEPLOYED:-$USE_SINGLE_MON_UNTIL_DEPLOYED_DEFAULT}"
 
 ###########################################################
 #   Install Cluster Role                                  #
@@ -38,6 +40,16 @@ helm repo add "csi" "$HELM_CHART"
 #   Install Operator                                      #
 ###########################################################
 
+echo "- Checking Ceph Cluster is already installed ..."
+if
+    kubectl get namespace --no-headers "rook-ceph" \
+        >/dev/null 2>/dev/null
+then
+    IS_FIRST=0
+else
+    IS_FIRST=1
+fi
+
 echo "- Installing CSI Operator ..."
 
 helm upgrade --install "rook-ceph" \
@@ -55,14 +67,100 @@ sleep 30
 
 echo "- Installing Storage Class ..."
 
+# do not update the number of monitors when re-deploying
+if [ "$IS_FIRST" -eq 0 ]; then
+    USE_SINGLE_MON_UNTIL_DEPLOYED="false"
+fi
+
+# tweaks - use single monitor node until ceph cluster is deployed
+# FIXME: Rook-Ceph on Flatcar OS is not working on mon > 1
+# See also: https://github.com/rook/rook/issues/10110
+if [ "$USE_SINGLE_MON_UNTIL_DEPLOYED" == "true" ]; then
+    NUM_MONS=$(yq ".cephClusterSpec.mon.count" "./values-cluster.yaml")
+    yq --inplace ".cephClusterSpec.mon.count = 1" "./values-cluster.yaml"
+fi
+
 helm upgrade --install "rook-ceph-cluster" \
     "csi/rook-ceph-cluster" \
     --create-namespace \
     --namespace "rook-ceph" \
     --values "./values-cluster.yaml"
 
-echo "- Waiting for deploying Storage Class ..."
-sleep 30
+echo -n "- Waiting for deploying Ceph Tools ... "
+kubectl --namespace "rook-ceph" rollout status deployment "rook-ceph-tools" >/dev/null
+echo "OK"
+
+echo -n "- Waiting for deploying Storage Classes ... "
+function wait_all_storage_class() {
+    while :; do
+        local COMPLETED=1
+        for storageclass in "blockpool" "filesystem" "objectstore"; do
+            local PHASE=$(
+                kubectl --namespace "rook-ceph" get "ceph$storageclass" "ceph-$storageclass" \
+                    --output jsonpath --template '{.status.phase}' \
+                    2>/dev/null
+            )
+            case "$PHASE" in
+            "Connected" | "Ready")
+                continue
+                ;;
+            *)
+                local COMPLETED=0
+                break
+                ;;
+            esac
+        done
+
+        if [ "$COMPLETED" -eq 1 ]; then
+            break
+        fi
+
+        # pass some times
+        sleep 5
+    done
+}
+wait_all_storage_class
+echo "OK"
+
+echo -n "- Waiting for deploying Ceph Cluster ... "
+function wait_ceph_cluster() {
+    while :; do
+        local PHASE=$(
+            kubectl --namespace "rook-ceph" get "cephcluster" "rook-ceph" \
+                --output jsonpath --template '{.status.phase}' \
+                2>/dev/null
+        )
+        case "$PHASE" in
+        "Ready")
+            continue
+            ;;
+        *)
+            break
+            ;;
+        esac
+
+        # pass some times
+        sleep 5
+    done
+}
+wait_ceph_cluster
+echo "OK"
+
+# tweaks - use single monitor nodes until ceph cluster is deployed
+# FIXME: Rook-Ceph on Flatcar OS is not working on mon > 1
+# See also: https://github.com/rook/rook/issues/10110
+if [ "$USE_SINGLE_MON_UNTIL_DEPLOYED" == "true" ]; then
+    if [ "$NUM_MONS" != "1" ]; then
+        yq --inplace ".cephClusterSpec.mon.count = $NUM_MONS" "./values-cluster.yaml"
+
+        helm upgrade --install "rook-ceph-cluster" \
+            "csi/rook-ceph-cluster" \
+            --create-namespace \
+            --namespace "rook-ceph" \
+            --values "./values-cluster.yaml"
+        wait_ceph_cluster
+    fi
+fi
 
 # Finished!
 echo "Installed CSI!"
