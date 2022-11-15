bf1fcbb2a7fd8308bbd3b06089451f02f36fce1b
Ho Kim <ho.kim@ulagbulag.io>
2022-11-15 02:18:58 +0000

    Add template: nvidia network operator

--- templates/bootstrap/bootstrap.sh
+++ templates/bootstrap/bootstrap.sh
@@ -15,6 +15,8 @@ BAREMETAL_CSI_DEFAULT="rook-ceph"
 BAREMETAL_CSI_INSTALLER_IMAGE_TEMPLATE_DEFAULT="quay.io/ulagbulag-village/netai-cloud-upgrade-csi-__BAREMETAL_CSI__:latest"
 BAREMETAL_GPU_INSTALLER_IMAGE_TEMPLATE_DEFAULT="quay.io/ulagbulag-village/netai-cloud-upgrade-gpu-__BAREMETAL_GPU__:latest"
 BAREMETAL_GPU_NVIDIA_DEFAULT="true"
+BAREMETAL_NETWORK_INSTALLER_IMAGE_TEMPLATE_DEFAULT="quay.io/ulagbulag-village/netai-cloud-upgrade-network-__BAREMETAL_NETWORK__:latest"
+BAREMETAL_NETWORK_NVIDIA_DEFAULT="true"
 CONTAINER_RUNTIME_DEFAULT="docker"
 IPIS_ENABLE_DEFAULT="true"
 IPIS_INSTALLER_IMAGE_DEFAULT="quay.io/ulagbulag-village/netai-cloud-upgrade-ipis:latest"
@@ -40,6 +42,8 @@ BAREMETAL_CSI="${BAREMETAL_CSI:-$BAREMETAL_CSI_DEFAULT}"
 BAREMETAL_CSI_INSTALLER_IMAGE_TEMPLATE="${BAREMETAL_CSI_INSTALLER_IMAGE_TEMPLATE:-$BAREMETAL_CSI_INSTALLER_IMAGE_TEMPLATE_DEFAULT}"
 BAREMETAL_GPU_INSTALLER_IMAGE_TEMPLATE="${BAREMETAL_GPU_INSTALLER_IMAGE_TEMPLATE:-$BAREMETAL_GPU_INSTALLER_IMAGE_TEMPLATE_DEFAULT}"
 BAREMETAL_GPU_NVIDIA="${BAREMETAL_GPU_NVIDIA:-$BAREMETAL_GPU_NVIDIA_DEFAULT}"
+BAREMETAL_NETWORK_INSTALLER_IMAGE_TEMPLATE="${BAREMETAL_NETWORK_INSTALLER_IMAGE_TEMPLATE:-$BAREMETAL_NETWORK_INSTALLER_IMAGE_TEMPLATE_DEFAULT}"
+BAREMETAL_NETWORK_NVIDIA="${BAREMETAL_NETWORK_NVIDIA:-$BAREMETAL_NETWORK_NVIDIA_DEFAULT}"
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-$CONTAINER_RUNTIME_DEFAULT}"
 IPIS_ENABLE="${IPIS_ENABLE:-$IPIS_ENABLE_DEFAULT}"
 IPIS_INSTALLER_IMAGE="${IPIS_INSTALLER_IMAGE:-$IPIS_INSTALLER_IMAGE_DEFAULT}"
@@ -68,6 +72,10 @@ BAREMETAL_CSI_INSTALLER_IMAGE="$(
 BAREMETAL_GPU_NVIDIA_INSTALLER_IMAGE="$(
     echo $BAREMETAL_GPU_INSTALLER_IMAGE_TEMPLATE |
         sed "s/__BAREMETAL_GPU__/nvidia/g"
+)"
+BAREMETAL_NETWORK_NVIDIA_INSTALLER_IMAGE="$(
+    echo $BAREMETAL_NETWORK_INSTALLER_IMAGE_TEMPLATE |
+        sed "s/__BAREMETAL_NETWORK__/nvidia/g"
 )"
 
 ###########################################################
@@ -528,6 +536,55 @@ function install_gpu_all() {
 }
 
 ###########################################################
+#   Install Network Operator                              #
+###########################################################
+
+# Define a Network operator installer function
+function install_network() {
+    local names="$1"
+    local node_first="$(echo $names | awk '{print $1}')"
+
+    # Configure environment variables
+    local baremetal_network="$3"
+    local baremetal_network_installer_image="$4"
+    local baremetal_network_name="$2"
+    local baremetal_network_namespace="network-${baremetal_network_name}"
+
+    # Check if Network operator already exists
+    local NEED_INSTALL=1
+    if [ "$baremetal_network" != "true" ]; then
+        echo -n "- Skipping installing Network operator ... "
+        local NEED_INSTALL=0
+    elif
+        "$CONTAINER_RUNTIME" exec "$node_first" \
+            kubectl get namespaces "$baremetal_network_namespace" \
+            >/dev/null 2>/dev/null
+    then
+        echo -n "- Using already installed $baremetal_network_name Network operator ... "
+        local NEED_INSTALL=0
+    fi
+
+    if [ "$NEED_INSTALL" -eq 1 ]; then
+        # Install Network operator
+        echo -n "- Installing $baremetal_network_name Network operator in background ... "
+        "$CONTAINER_RUNTIME" run --detach --rm \
+            --name "network-operator-installer-$baremetal_network_name" \
+            --net "host" \
+            --volume "$KUBERNETES_CONFIG:/root/.kube:ro" \
+            "$baremetal_network_installer_image" \
+            >/dev/null
+    fi
+
+    # Finished!
+    echo "OK"
+}
+
+# Define Network operators installer function
+function install_network_all() {
+    install_network "$1" "nvidia" "$BAREMETAL_NETWORK_NVIDIA" "$BAREMETAL_NETWORK_NVIDIA_INSTALLER_IMAGE"
+}
+
+###########################################################
 #   Install IPIS                                          #
 ###########################################################
 
@@ -591,6 +648,9 @@ function main() {
 
     # Install GPU operators
     install_gpu_all $KUBESPRAY_NODES
+
+    # Install Network operators
+    install_network_all $KUBESPRAY_NODES
 
     # Install an IPIS cluster
     install_ipis_cluster $KUBESPRAY_NODES
