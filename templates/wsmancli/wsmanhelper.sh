d7d3e0e587e16f19d563c33e8270e1028be39dfd
Ho Kim <ho.kim@ulagbulag.io>
2023-03-02 13:20:01 +0000

    Fix a typo in wsmanhelper

--- templates/wsmancli/wsmanhelper.sh
+++ templates/wsmancli/wsmanhelper.sh
@@ -13,15 +13,15 @@ set -x
 ###########################################################
 
 # Parse from command-line arguments
-AWT_KIND="$1"
-AWT_ACTION="$2"
+AMT_KIND="$1"
+AMT_ACTION="$2"
 
 # Parse variables
-AWT_INPUT_FILENAME="${TEMPLATES_HOME}/${AWT_KIND}-${AWT_ACTION}.xml"
-AWT_PORT="${AWT_PORT:-16992}"
+AMT_INPUT_FILENAME="${TEMPLATES_HOME}/${AMT_KIND}-${AMT_ACTION}.xml"
+AMT_PORT="${AMT_PORT:-16992}"
 
-AWT_METHOD="$(cat "${AWT_INPUT_FILENAME}" | grep -Po '\<input\:\K[0-9A-Za-z]+' | head -n 1)"
-AWT_RESOURCE_URI="$(cat "${AWT_INPUT_FILENAME}" | grep -Po 'xmlns\:input\=\"\K[0-9A-Za-z\.\_\-\:\/]+' | head -n 1)"
+AMT_METHOD="$(cat "${AMT_INPUT_FILENAME}" | grep -Po '\<input\:\K[0-9A-Za-z]+' | head -n 1)"
+AMT_RESOURCE_URI="$(cat "${AMT_INPUT_FILENAME}" | grep -Po 'xmlns\:input\=\"\K[0-9A-Za-z\.\_\-\:\/]+' | head -n 1)"
 
 ###########################################################
 #   Execute                                               #
@@ -29,9 +29,9 @@ AWT_RESOURCE_URI="$(cat "${AWT_INPUT_FILENAME}" | grep -Po 'xmlns\:input\=\"\K[0
 
 exec wsman invoke \
     -a "RequestPowerStateChange" \
-    -J "${AWT_INPUT_FILENAME}" \
-    -h "${AWT_HOSTNAME}" \
-    -P "${AWT_PORT}" \
-    -p "${AWT_PASSWORD}" \
-    -u "${AWT_USERNAME}" \
-    "${AWT_RESOURCE_URI}"
+    -J "${AMT_INPUT_FILENAME}" \
+    -h "${AMT_HOSTNAME}" \
+    -P "${AMT_PORT}" \
+    -p "${AMT_PASSWORD}" \
+    -u "${AMT_USERNAME}" \
+    "${AMT_RESOURCE_URI}"
