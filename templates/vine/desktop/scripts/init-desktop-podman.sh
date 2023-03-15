fe9f21e6459c20d5af88da7f5889c99a9c37ce19
Ho Kim <ho.kim@ulagbulag.io>
2023-03-15 16:01:17 +0900

    Add podman+docker in kubernetes support for vine

--- templates/vine/desktop/scripts/init-desktop-podman.sh
+++ templates/vine/desktop/scripts/init-desktop-podman.sh
@@ -11,3 +11,9 @@ set -x
 # Copy podman containers configuration file
 mkdir -p "${HOME}/.config/containers"
 cp /etc/containers/podman-containers.conf "${HOME}/.config/containers/containers.conf"
+
+# Initialize rootless podman
+podman system migrate
+
+# Generate a CDI specification that refers to all NVIDIA devices
+nvidia-ctk cdi generate --device-name-strategy=type-index --format=json > /etc/cdi/nvidia.json
