c1f345cbc7c1ded39b158ec0625d259fe00e496c
Ho Kim <ho.kim@ulagbulag.io>
2023-06-06 14:16:46 +0900

    Fix a bug in vine controller

--- vine/controller/src/ctx/user_session.rs
+++ vine/controller/src/ctx/user_session.rs
@@ -6,6 +6,7 @@ use async_trait::async_trait;
 use k8s_openapi::api::core::v1::Node;
 use kube::{runtime::controller::Action, Error, ResourceExt};
 use log::{info, warn};
+use vine_api::user::UserCrd;
 use vine_session::SessionManager;
 
 #[derive(Default)]
@@ -25,18 +26,26 @@ impl ::ark_core_k8s::manager::Ctx for Ctx {
     where
         Self: Sized,
     {
-        let session_manager =
-            match SessionManager::try_new(Self::NAMESPACE.into(), manager.kube.clone()).await {
-                Ok(session_manager) => session_manager,
-                Err(e) => {
-                    warn!("failed to creata a SessionManager: {e}");
-                    return Ok(Action::requeue(
-                        <Self as ::ark_core_k8s::manager::Ctx>::FALLBACK,
-                    ));
-                }
-            };
-
         let name = data.name_any();
+        let namespace = match data.labels().get(::ark_api::consts::LABEL_BIND_BY_USER) {
+            Some(user_name) => UserCrd::user_namespace_with(user_name),
+            None => {
+                info!("skipping unbinding node ({name}): user not found");
+                return Ok(Action::requeue(
+                    <Self as ::ark_core_k8s::manager::Ctx>::FALLBACK,
+                ));
+            }
+        };
+
+        let session_manager = match SessionManager::try_new(namespace, manager.kube.clone()).await {
+            Ok(session_manager) => session_manager,
+            Err(e) => {
+                warn!("failed to creata a SessionManager: {e}");
+                return Ok(Action::requeue(
+                    <Self as ::ark_core_k8s::manager::Ctx>::FALLBACK,
+                ));
+            }
+        };
 
         match session_manager.try_delete(&data).await {
             Ok(Some(user_name)) => {
