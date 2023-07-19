1121a58e57628af4ebe989a4df831780d7f7c128
Ho Kim <ho.kim@ulagbulag.io>
2023-07-19 18:10:48 +0900

    Fix a bug in screen resizing

--- templates/vine/desktop/scripts/init-desktop-xfce4.sh
+++ templates/vine/desktop/scripts/init-desktop-xfce4.sh
@@ -8,5 +8,35 @@ set -e -o pipefail
 # Verbose
 set -x
 
+# Configure screen
+function update_screen() {
+    echo "Finding displays..."
+    screens="$(xrandr --current | grep ' connected ' | awk '{print $1}')"
+    if [ "x${screens}" == "x" ]; then
+        echo 'Display not found!'
+        return
+    fi
+
+    for screen in $(echo -en "${screens}"); do
+        echo "Fixing screen size to perferred (${screen})..."
+        screen_resolution="$(xrandr --current | awk "/HDMI-0 *connected /{getline;print \$1}")"
+        screen_refresh_rate="$(xrandr --current | awk "/HDMI-0 *connected /{getline;print \$2}")"
+
+        echo "* Resolution = '${screen_resolution}'"
+        echo "* Refresh Rate = '${screen_refresh_rate}'"
+        xmlstarlet edit \
+            --inplace \
+            --update "/channel/property/property[@name='${screen}']/property[@name='Resolution']/@value" \
+            --value "${screen_resolution}"
+        xmlstarlet edit \
+            --inplace \
+            --update "/channel/property/property[@name='${screen}']/property[@name='RefreshRate']/@value" \
+            --value "${screen_refresh_rate}"
+    done
+}
+
+# Apply
+update_screen
+
 # Run desktop environment
 exec /usr/bin/dbus-launch xfce4-session
--- utils/batch-add-user.sh
+++ utils/batch-add-user.sh
@@ -11,24 +11,46 @@ set -e -o pipefail
 ###########################################################
 
 # Configure default environment variables
-CSV_PATH_DEFAULT="./list.csv"
+CSV_PATH_DEFAULT="./users.csv"
 
 # Configure environment variables
 CSV_PATH="${CSV_PATH:-$CSV_PATH_DEFAULT}"
+TEMPLATE="
+---
+apiVersion: vine.ulagbulag.io/v1alpha1
+kind: User
+metadata:
+  name: __ID__
+spec:
+  contact: {}
+  detail: {}
+  name: __NAME__
+---
+apiVersion: vine.ulagbulag.io/v1alpha1
+kind: UserBoxQuotaBinding
+metadata:
+  name: __ID__-desktop
+spec:
+  quota: __QUOTA__
+  user: __ID__
+"
 
 ###########################################################
 #   Label all noxes with given Aliases                    #
 ###########################################################
 
-echo 'Labeling boxes'
+echo 'Creating nodes'
 for line in $(cat "${CSV_PATH}" | tail '+2'); do
-    box_name="$(echo "${line}" | cut '-d,' -f1)"
-    box_alias="$(echo "${line}" | cut '-d,' -f2)"
+    user_id="$(echo "${line}" | cut '-d,' -f1)"
+    user_name="$(echo "${line}" | cut '-d,' -f2)"
+    user_quota="$(echo "${line}" | cut '-d,' -f3)"
 
-    echo -n "* ${box_alias} -> "
-    kubectl patch boxes "${box_name}" \
-        --type 'merge' \
-        --patch "{\"metadata\":{\"labels\":{\"dash.ulagbulag.io/alias\":\"${box_alias}\"}}}"
+    echo -n "* ${user_name} (${user_quota}) -> "
+    echo "${TEMPLATE}" |
+        sed "s/__ID__/${user_id}/g" |
+        sed "s/__NAME__/${user_name}/g" |
+        sed "s/__QUOTA__/${user_quota}/g" |
+        kubectl apply -f -
 done
 
 ###########################################################
