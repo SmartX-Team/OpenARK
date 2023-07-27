50688d11f600acf7c769aaebdbc83c58383baf0f
Ho Kim <ho.kim@ulagbulag.io>
2023-07-27 21:06:11 +0900

    Add patch-template-optimize-cri.sh

---
+++ utils/patch-template-optimize-cri.sh
@@ -0,0 +1,30 @@
+# Remove temporary user space
+sudo rm -rf /opt/vdi/tenants/remote/vine-session-*
+
+# Grow root partition size
+if ip a | grep wlp >/dev/null 2>/dev/null; then
+    sudo dnf install -y cloud-utils-growpart
+    sudo growpart /dev/nvme0n1 3
+    sudo resize2fs /dev/nvme0n1p3
+fi
+
+# Allow containerd to pull images from insecure local private registry
+if ! sudo cat /etc/containerd/config.toml | grep 'registry.ark.svc.ops.openark' >/dev/null 2>/dev/null; then
+    echo '        [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.ark.svc.ops.openark"]' |
+        sudo tee -a /etc/containerd/config.toml
+    echo '          endpoint = ["http://registry.ark.svc.ops.openark"]' |
+        sudo tee -a /etc/containerd/config.toml
+    echo '      [plugins."io.containerd.grpc.v1.cri".registry.configs]' |
+        sudo tee -a /etc/containerd/config.toml
+    echo '        [plugins."io.containerd.grpc.v1.cri".registry.configs."registry.ark.svc.ops.openark"]' |
+        sudo tee -a /etc/containerd/config.toml
+    echo '          [plugins."io.containerd.grpc.v1.cri".registry.configs."registry.ark.svc.ops.openark".tls]' |
+        sudo tee -a /etc/containerd/config.toml
+    echo '            insecure_skip_verify = true' |
+        sudo tee -a /etc/containerd/config.toml
+fi
+
+# Restart containerd if Mobile
+if ip a | grep wlp >/dev/null 2>/dev/null; then
+    sudo systemctl restart containerd
+fi
