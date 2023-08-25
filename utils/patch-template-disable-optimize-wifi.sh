73a8d7e096fd4e19cd2c13ced11e598f35ec96e0
Ho Kim <ho.kim@ulagbulag.io>
2023-08-25 22:09:12 +0900

    Update patch-template-disable-optimize-wifi.sh

--- utils/patch-template-disable-optimize-wifi.sh
+++ utils/patch-template-disable-optimize-wifi.sh
@@ -1,6 +1,6 @@
 sudo systemctl disable --now optimize-wifi.service
 
 sudo chmod u+w /etc/NetworkManager/system-connections/10-kiss-enable-master.nmconnection || true
-sudo sed -i 's/bssid/#\1/g' /etc/NetworkManager/system-connections/10-kiss-enable-master.nmconnection || true
+sudo sed -i 's/bssid/#\0/g' /etc/NetworkManager/system-connections/10-kiss-enable-master.nmconnection || true
 sudo chmod u-w /etc/NetworkManager/system-connections/10-kiss-enable-master.nmconnection || true
 sudo systemctl restart NetworkManager.service
