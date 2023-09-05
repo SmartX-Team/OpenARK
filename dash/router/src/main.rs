049ffbbbc84c18fb26a5e86bb94a8aaa35462183
Ho Kim <ho.kim@ulagbulag.io>
2023-09-05 17:57:59 +0900

    Begin implementation of dash router

--- dash/router/src/main.rs
+++ dash/router/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+
+use std::net::SocketAddr;
+
+use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
+use anyhow::Result;
+use ark_core::{env::infer, logger};
+use kube::Client;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().json("dash-router")
+}
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok().json("healthy")
+}
+
+#[actix_web::main]
+async fn main() {
+    async fn try_main() -> Result<()> {
+        // Initialize kubernetes client
+        let addr =
+            infer::<_, SocketAddr>("BIND_ADDR").unwrap_or_else(|_| "0.0.0.0:80".parse().unwrap());
+        let client = Data::new(Client::try_default().await?);
+
+        // Start web server
+        HttpServer::new(move || {
+            let app = App::new().app_data(Data::clone(&client));
+            app.service(index).service(health)
+        })
+        .bind(addr)
+        .unwrap_or_else(|e| panic!("failed to bind to {addr}: {e}"))
+        .run()
+        .await
+        .map_err(Into::into)
+    }
+
+    logger::init_once();
+    try_main().await.expect("running a server")
 }
