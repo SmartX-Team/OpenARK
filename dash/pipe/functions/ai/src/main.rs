3182f5294a5dd644671f981486f60f58e9d4d353
Ho Kim <ho.kim@ulagbulag.io>
2023-10-03 16:06:03 +0900

    New package: dash-pipe-function-ai

---
+++ dash/pipe/functions/ai/src/main.rs
@@ -0,0 +1,56 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use clap::{ArgAction, Parser};
+use dash_pipe_provider::{PipeArgs, PipeMessage, PipeMessages, PipePayload};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+fn main() {
+    PipeArgs::<Function>::from_env().loop_forever()
+}
+
+#[derive(Clone, Debug, Parser, Serialize, Deserialize)]
+pub struct FunctionArgs {
+    #[arg(long, env = "PIPE_IDENTITY_WRITE_TO_PERSISTENT_STORAGE", action = ArgAction::SetTrue)]
+    #[serde(default)]
+    write_to_persistent_storage: Option<bool>,
+}
+
+pub struct Function {
+    args: FunctionArgs,
+}
+
+#[async_trait]
+impl ::dash_pipe_provider::Function for Function {
+    type Args = FunctionArgs;
+    type Input = Value;
+    type Output = Value;
+
+    async fn try_new(args: &<Self as ::dash_pipe_provider::Function>::Args) -> Result<Self> {
+        Ok(Self { args: args.clone() })
+    }
+
+    async fn tick(
+        &mut self,
+        inputs: PipeMessages<<Self as ::dash_pipe_provider::Function>::Input>,
+    ) -> Result<PipeMessages<<Self as ::dash_pipe_provider::Function>::Output>> {
+        match self.args.write_to_persistent_storage {
+            Some(true) => Ok(match inputs {
+                PipeMessages::None => PipeMessages::None,
+                PipeMessages::Single(message) => PipeMessages::Single(pack_payload(message)?),
+                PipeMessages::Batch(messages) => PipeMessages::Batch(
+                    messages
+                        .into_iter()
+                        .map(pack_payload)
+                        .collect::<Result<_>>()?,
+                ),
+            }),
+            Some(false) | None => Ok(inputs),
+        }
+    }
+}
+
+fn pack_payload(mut message: PipeMessage<Value>) -> Result<PipeMessage<Value>> {
+    message.payloads = vec![PipePayload::new("test".into(), message.to_json_bytes()?)];
+    Ok(message)
+}
