513f686a1a3f773f092c139c1e71a356d2396e76
Ho Kim <ho.kim@ulagbulag.io>
2023-10-08 19:49:28 +0000

    Add github arc devops templates

---
+++ templates/devops/arc/install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Copyright (c) 2022 Ho Kim (ho.kim@ulagbulag.io). All rights reserved.
+# Use of this source code is governed by a GPL-3-style license that can be
+# found in the LICENSE file.
+
+# Prehibit errors
+set -e -o pipefail
+# Verbose
+set -x
+
+###########################################################
+#   Configuration                                         #
+###########################################################
+
+# Configure default environment variables
+HELM_CHART_DEFAULT="https://actions-runner-controller.github.io/actions-runner-controller"
+NAMESPACE_DEFAULT="devops-arc"
+
+# Set environment variables
+HELM_CHART="${HELM_CHART:-$HELM_CHART_DEFAULT}"
+NAMESPACE="${NAMESPACE:-$NAMESPACE_DEFAULT}"
+
+###########################################################
+#   Check Environment Variables                           #
+###########################################################
+
+if [ "x${GITHUB_TOKEN}" == "x" ]; then
+    echo 'Skipping installation: "GITHUB_TOKEN" not set'
+    exit 0
+fi
+
+###########################################################
+#   Configure Helm Channel                                #
+###########################################################
+
+echo "- Configuring Helm channel ... "
+
+helm repo add "${NAMESPACE}" "${HELM_CHART}"
+
+###########################################################
+#   Checking if Operator is already installed             #
+###########################################################
+
+echo "- Checking Operator is already installed ... "
+if
+    kubectl get namespace --no-headers "${NAMESPACE}" \
+        >/dev/null 2>/dev/null
+then
+    IS_FIRST=0
+else
+    IS_FIRST=1
+fi
+
+###########################################################
+#   Install Operator                                      #
+###########################################################
+
+echo "- Installing Operator ... "
+
+helm upgrade --install "github-actions-runner-controller" \
+    "${NAMESPACE}/actions-runner-controller" \
+    --create-namespace \
+    --namespace "${NAMESPACE}" \
+    --set authSecret.github_token="${GITHUB_TOKEN}" \
+    --values "./values-operator.yaml"
+
+# Finished!
+echo "Installed!"
