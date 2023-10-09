fa100c7bc6b99de31ed54ac8fb2a3256c8484250
Ho Kim <ho.kim@ulagbulag.io>
2023-10-09 08:55:20 +0000

    Add pvc option for dash nats

--- templates/dash/nats/install.sh
+++ templates/dash/nats/install.sh
@@ -15,10 +15,12 @@ set -x
 # Configure default environment variables
 HELM_CHART_DEFAULT="https://nats-io.github.io/k8s/helm/charts"
 NAMESPACE_DEFAULT="dash"
+NATS_ENABLE_PVC_DEFAULT="true"
 
 # Set environment variables
 HELM_CHART="${HELM_CHART:-$HELM_CHART_DEFAULT}"
 NAMESPACE="${NAMESPACE:-$NAMESPACE_DEFAULT}"
+NATS_ENABLE_PVC="${NATS_ENABLE_PVC:-$NATS_ENABLE_PVC_DEFAULT}"
 
 # Parse from CoreDNS
 export CLUSTER_NAME="$(
@@ -69,6 +71,7 @@ helm upgrade --install "nats" \
     --create-namespace \
     --namespace "${NAMESPACE}" \
     --set config.cluster.routeURLs.k8sClusterDomain="${CLUSTER_NAME}" \
+    --set config.jetstream.fileStore.pvc.enabled="${NATS_ENABLE_PVC}" \
     --values "./values.yaml"
 
 # Finished!
