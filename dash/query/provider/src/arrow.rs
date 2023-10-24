37e4483df6b3abd34adc0e47a633a516a98bd628
Ho Kim <ho.kim@ulagbulag.io>
2023-10-24 13:53:18 +0000

    Fix a bug in flattening nullable fields

--- dash/query/provider/src/arrow.rs
+++ dash/query/provider/src/arrow.rs
@@ -19,7 +19,7 @@ impl IntoFlattened for RecordBatch {
 impl IntoFlattened for StructArray {
     fn into_flattened(self) -> Self {
         let mut flattened_fields = vec![];
-        flatten_struct_array(&mut flattened_fields, "", Arc::new(self), false);
+        flatten_struct_array(&mut flattened_fields, "", Arc::new(self));
         StructArray::from(flattened_fields)
     }
 }
@@ -28,23 +28,25 @@ fn flatten_struct_array(
     flattened_fields: &mut Vec<(FieldRef, ArrayRef)>,
     name: &str,
     array: ArrayRef,
-    nullable: bool,
 ) {
     match array.as_struct_opt() {
         Some(array) => {
-            let (fields, arrays, nulls) = array.clone().into_parts();
+            let (fields, arrays, _nulls) = array.clone().into_parts();
             fields.into_iter().zip(arrays).for_each(|(field, array)| {
                 let name = if name.is_empty() {
                     field.name().into()
                 } else {
                     format!("{name}.{}", field.name())
                 };
-                let nullable = nulls.is_some();
-                flatten_struct_array(flattened_fields, &name, array, nullable)
+                flatten_struct_array(flattened_fields, &name, array)
             })
         }
         None => {
-            let field = Arc::new(Field::new(name, array.data_type().clone(), nullable));
+            let field = Arc::new(Field::new(
+                name,
+                array.data_type().clone(),
+                array.is_nullable(),
+            ));
             flattened_fields.push((field, array));
         }
     }
