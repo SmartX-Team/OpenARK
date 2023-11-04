a5ee571e4e1d766bb8a1c36fa5e9885e315e72bb
Ho Kim <ho.kim@ulagbulag.io>
2023-11-04 12:49:54 +0000

    vpn: Add sample  template

---
+++ templates/vpn/README.md
@@ -0,0 +1,6 @@
+# VPN Templates
+
+## Warnings
+
+It is for testing only and **SHOULD BE FINE-TUNED** for your environment.
+There is no warranty of any kind for any disadvantages arising from not being aware of this.
---
+++ templates/vpn/softether/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Copyright (c) 2022 Ho Kim (ho.kim@ulagbulag.io). All rights reserved.
+# Use of this source code is governed by a GPL-3-style license that can be
+# found in the LICENSE file.
+
+# Prehibit errors
+set -e -o pipefail
+# Verbose
+set -x
+
+###########################################################
+#   Check Environment Variables                           #
+###########################################################
+
+if [ "x${ADMIN_PASSWORD}" == "x" ]; then
+    echo 'Skipping installation: "ADMIN_PASSWORD" not set'
+    exit 0
+fi
+
+if [ "x${LOADBALANCER_IP}" == "x" ]; then
+    echo 'Skipping installation: "LOADBALANCER_IP" not set'
+    exit 0
+fi
+
+###########################################################
+#   Install Server                                        #
+###########################################################
+
+echo "- Installing Server ... "
+
+cat './templates.yaml' |
+    sed "s/__ADMIN_PASSWORD__/${ADMIN_PASSWORD}/g" |
+    sed "s/__LOADBALANCER_IP__/${LOADBALANCER_IP}/g" |
+    kubectl apply -f -
+
+# Finished!
+echo "Installed!"
