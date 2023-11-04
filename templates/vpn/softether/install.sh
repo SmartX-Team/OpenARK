43bfc57e645a2ba2cba63f40ecf803f9db07ff8e
Ho Kim <ho.kim@ulagbulag.io>
2023-11-04 12:51:42 +0000

    vpn: Improve security on deploying

--- templates/vpn/softether/install.sh
+++ templates/vpn/softether/install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# Copyright (c) 2022 Ho Kim (ho.kim@ulagbulag.io). All rights reserved.
+# Copyright (c) 2023 Ho Kim (ho.kim@ulagbulag.io). All rights reserved.
 # Use of this source code is governed by a GPL-3-style license that can be
 # found in the LICENSE file.
 
 # Prehibit errors
 set -e -o pipefail
-# Verbose
-set -x
 
 ###########################################################
 #   Check Environment Variables                           #
