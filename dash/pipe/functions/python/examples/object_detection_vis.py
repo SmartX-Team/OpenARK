480e0778333100eb544e7f6fcad1bf0e92b5bfc1
Ho Kim <ho.kim@ulagbulag.io>
2023-12-20 11:06:28 +0000

    feat(dash): add image frame ignore option support

--- dash/pipe/functions/python/examples/object_detection_vis.py
+++ dash/pipe/functions/python/examples/object_detection_vis.py
@@ -2,6 +2,7 @@
 
 
 import io
+import os
 from typing import Any
 
 import cv2
@@ -12,8 +13,13 @@ from PIL import Image
 class FrameCursor:
     def __init__(self) -> None:
         self._current_key = None
+        self._ignore_frame = os.environ.get(
+            'PIPE_PYTHON_IGNORE_FRAME', 'false') == 'true'
 
     def update(self, key: Any) -> bool:
+        if self._ignore_frame:
+            return True
+
         if self._current_key is not None and self._current_key >= key:
             return False
         self._current_key = key
@@ -25,7 +31,7 @@ frame_cursor = FrameCursor()
 
 
 # show window
-WINDOW_NAME='Object Detection'
+WINDOW_NAME = 'Object Detection'
 cv2.namedWindow(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN)
 
 
@@ -35,11 +41,12 @@ def tick(inputs: list[Any]) -> list[Any]:
         return []
 
     # load payloads
-    input_set: list[tuple[int, int, str, bytes]] = [
+    input_set: list[tuple[int, int, str, str, bytes]] = [
         (
             batch_idx,
             payload_idx,
             key,
+            input.reply,
             payload,
         )
         for batch_idx, input in enumerate(inputs)
@@ -53,26 +60,32 @@ def tick(inputs: list[Any]) -> list[Any]:
     # load inputs
     input_images = [
         (key, Image.open(io.BytesIO(payload)))
-        for (_, _, key, payload) in input_set
+        for (_, _, key, _, payload) in input_set
     ]
 
     # skip if already passed frame
     if not frame_cursor.update(input_images[0][0]):
         return []
 
-    # draw
+    # draw image
     input_image = cv2.cvtColor(np.array(input_images[0][1]), cv2.COLOR_RGB2BGR)
-    for input_object in inputs[0].value['value']:
-        input_object_box: dict[str, float] = input_object['box']
-        input_object_color = (255, 0, 0)
-        input_object_name: str = input_object['name']
-        pt_min = (int(input_object_box['x1']), int(input_object_box['y1']))
-        pt_max = (int(input_object_box['x2']), int(input_object_box['y2']))
 
-        input_image = cv2.rectangle(
-            input_image, pt_min, pt_max, input_object_color, 1)
-        input_image = cv2.putText(input_image, input_object_name,
-                                  pt_min, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+    # draw bbox
+    value = inputs[0].value.get('value', None)
+    if not value is None:
+        for input_object in value:
+            input_object_box: dict[str, float] = input_object['box']
+            input_object_color = (255, 0, 0)
+            input_object_name: str = input_object['name']
+            pt_min = (int(input_object_box['x1']), int(input_object_box['y1']))
+            pt_max = (int(input_object_box['x2']), int(input_object_box['y2']))
+
+            input_image = cv2.rectangle(
+                input_image, pt_min, pt_max, input_object_color, 1)
+            input_image = cv2.putText(input_image, input_object_name,
+                                      pt_min, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+
+    # show
     cv2.imshow(WINDOW_NAME, input_image)
     cv2.waitKey(1)
 
