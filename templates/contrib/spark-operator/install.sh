0f58fe45f34affa730308ad933d2c6323340f8a0
Ho Kim <ho.kim@ulagbulag.io>
2024-07-18 12:31:32 +0000

    feat(contrib): add initial spark-operator support

---
+++ templates/contrib/spark-operator/install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Copyright (c) 2024 Ho Kim (ho.kim@ulagbulag.io). All rights reserved.
+# Use of this source code is governed by a GPL-3-style license that can be
+# found in the LICENSE file.
+
+# Prehibit errors
+set -e -o pipefail
+# Verbose
+set -x
+
+###########################################################
+#   Configuration                                         #
+###########################################################
+
+# Configure default environment variables
+HELM_CHART_DEFAULT="https://kubeflow.github.io/spark-operator"
+NAMESPACE_DEFAULT="spark-operator"
+
+# Set environment variables
+HELM_CHART="${HELM_CHART:-$HELM_CHART_DEFAULT}"
+NAMESPACE="${NAMESPACE:-$NAMESPACE_DEFAULT}"
+
+###########################################################
+#   Configure Helm Channel                                #
+###########################################################
+
+echo "- Configuring Helm channel ... "
+
+helm repo add "${NAMESPACE}" "${HELM_CHART}"
+
+###########################################################
+#   Checking if Operator is already installed             #
+###########################################################
+
+echo "- Checking Operator is already installed ... "
+if
+    kubectl get namespace --no-headers "${NAMESPACE}" \
+        >/dev/null 2>/dev/null
+then
+    IS_FIRST=0
+else
+    IS_FIRST=1
+fi
+
+###########################################################
+#   Install Operator                                      #
+###########################################################
+
+echo "- Installing Operator ... "
+
+helm upgrade --install "spark-operator" \
+    "${NAMESPACE}/spark-operator" \
+    --create-namespace \
+    --namespace "${NAMESPACE}" \
+    --values "./values-operator.yaml"
+
+# Finished!
+echo "Installed!"
